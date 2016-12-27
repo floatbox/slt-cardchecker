0b7dc7e8f4bacdb1735b93963bc05b0cad0d5d7f
Andrey Arefiev <arefiev@gmail.com>
2016-12-27 14:05:49 +0300

    Add cardtype checking

--- card_check.rb
+++ card_check.rb
@@ -8,6 +8,21 @@ class CardCheck
     luhn_algorithm
   end
 
+  def cardtype
+    case
+    when /^(34|37)/ =~ @cardnumber && number_length == 15
+      'AMEX'
+    when /^6011/ =~ @cardnumber && number_length == 16
+      'Discover'
+    when /^5[1-5]/ =~ @cardnumber && number_length == 16
+      'MasterCard'
+    when /^4/ =~ @cardnumber && (number_length == 13 || number_length == 16)
+      'Visa'
+    else
+      'Unknown'
+    end
+  end
+
   private
 
   def luhn_algorithm
@@ -16,5 +31,9 @@ class CardCheck
       check_digits += i.odd? ? digit : (digit.to_i * 2).to_s
     end
     (check_digits.split('').map(&:to_i).reduce(:+) % 10).zero?
+  end
+
+  def number_length
+    @number_length ||= @cardnumber.size
   end
 end
--- card_check_test.rb
+++ card_check_test.rb
@@ -17,5 +17,47 @@ describe CardCheck do
     end
   end
 
+  describe "Cardnumber 3417 1234 5678 911" do
+    it "is AMEX" do
+      @card = CardCheck.new '3417 1234 5678 911'
+      @card.cardtype.must_equal 'AMEX'
+    end
+  end
+
+  describe "Cardnumber 6011 1234 5678 9112" do
+    it "is Discover" do
+      @card = CardCheck.new '6011 1234 5678 9112'
+      @card.cardtype.must_equal 'Discover'
+    end
+  end
+
+  describe "Cardnumber 5117 1234 5678 9112" do
+    it "is MasterCard" do
+      @card = CardCheck.new '5117 1234 5678 9112'
+      @card.cardtype.must_equal 'MasterCard'
+    end
+  end
+
+  describe "Cardnumber 4417 1234 5678 9112" do
+    it "is Visa" do
+      @card = CardCheck.new '4417 1234 5678 9112'
+      @card.cardtype.must_equal 'Visa'
+    end
+  end
+
+  describe "Cardnumber 4417 1234 5678 9" do
+    it "is Visa" do
+      @card = CardCheck.new '4417 1234 5678 9'
+      @card.cardtype.must_equal 'Visa'
+    end
+  end
+
+  describe "Cardnumber 5517 1234 5678 9" do
+    it "is Unknown" do
+      @card = CardCheck.new '5517 1234 5678 9'
+      @card.cardtype.must_equal 'Unknown'
+    end
+  end
+
 end
 
