42f16ba0f1c494d6b94606760c8763b796ee127e
Andrey Arefiev <arefiev@gmail.com>
2016-12-27 13:46:03 +0300

    Validation by luhn_algorithm

--- card_check.rb
+++ card_check.rb
@@ -1,10 +1,20 @@
 # Checking Credit Cards by the Luhn algorithm
 class CardCheck
-  def initialize(number)
-    @cardnumber = number
+  def initialize(number_string)
+    @cardnumber = number_string.gsub(/\D/, '')
   end
 
   def valid?
-    true
+    luhn_algorithm
+  end
+
+  private
+
+  def luhn_algorithm
+    check_digits = ''
+    @cardnumber.split('').reverse.each_with_index do |digit, i|
+      check_digits += i.odd? ? digit : (digit.to_i * 2).to_s
+    end
+    (check_digits.split('').map(&:to_i).reduce(:+) % 10).zero?
   end
 end
